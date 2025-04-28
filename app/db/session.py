7e53da16bec5d3486b67dcafebc3ec4b8f45341c
insil3nc3 <minecrafttvem@gmail.com>
2025-04-28 18:04:27 +0200

    created db local session, created docker-compose

--- app/db/session.py
+++ app/db/session.py
@@ -1,1 +1,19 @@
 """connection to db"""
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from app.core.config import settings
+
+SQLALCHEMY_DATABASE_URL = settings.database_url
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+def get_db():
+    session = SessionLocal()
+    try:
+        yield session
+    finally:
+        session.close()
