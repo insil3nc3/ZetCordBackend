4356738b03b7fe41414cdb00ed2cbcd230f7c4dd
insil3nc3 <minecrafttvem@gmail.com>
2025-05-02 19:47:56 +0200

    init_db() recreating db now

--- app/db/postgres/init_db.py
+++ app/db/postgres/init_db.py
@@ -11,6 +11,7 @@ async def init_db():
     engine = create_async_engine(SQLALCHEMY_DATABASE_URL, echo=True)
 
     async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
 
     print("База данных успешно создана!")
