f60fc61951f7acdfaa960e53c150b8d5607fa815
insil3nc3 <minecrafttvem@gmail.com>
2025-05-02 19:49:51 +0200

    old methods updated, created get_avat_by_id method

--- app/repository/postgres/crud.py
+++ app/repository/postgres/crud.py
@@ -1,19 +1,25 @@
 """temporarily: file to get some information from DB"""
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine
-from app.models.models import Users
-
+from sqlalchemy import select
+from app.models.models import Users, UserProfile
+from sqlalchemy.ext.asyncio import AsyncSession
+from fastapi import HTTPException
 base_url = "postgresql://user_user:user@localhost:5432/user_db"
-engine = create_engine(base_url)
-Session = sessionmaker(engine)
-session = Session()
-users = session.query(Users).all()
-for user in users:
-    print(f"id: {user.id}")
-    print(f"email: {user.email}")
-    print(f"password: {user.hashed_password}")
-    print(f"role: {user.role}")
-    print(f"refresh_token: {user.refresh_token}")
-#     session.delete(user)
-#
-# session.commit()
+
+async def get_user_by_email(user_email: str, session: AsyncSession) -> Users | None:
+    result = await session.execute(select(Users).where(Users.email == user_email))
+    if result:
+        return result.scalars().first()
+
+async def get_user_profile_by_email(email: str, session: AsyncSession):
+    user = await get_user_by_email(email, session)
+    result = await session.execute(select(UserProfile).where(UserProfile.user_id == user.id))
+    if result:
+        return result.scalars().first()
+
+async def get_avatar_by_id(profile_id: int, session: AsyncSession):
+    result = await session.execute(select(UserProfile).where(UserProfile.id == profile_id))
+    profile = result.scalars().first()
+    if not profile:
+        raise HTTPException(status_code=404, detail="Avatar not found")
+    return profile.avatar
