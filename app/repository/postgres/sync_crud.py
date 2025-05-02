f1884367b3ec89cb227277b7f59cc1a00fc8dfe0
insil3nc3 <minecrafttvem@gmail.com>
2025-05-02 19:50:32 +0200

    temporarily file to check data in db

--- app/repository/postgres/sync_crud.py
+++ app/repository/postgres/sync_crud.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from app.models.models import Users
+from app.models.models import Users, UserProfile
 
 base_url = "postgresql://user_user:user@localhost:5432/user_db"
 engine = create_engine(base_url)
@@ -9,4 +9,7 @@ session = Session()
 
 users = session.query(Users).all()
 for user in users:
-    print(user.id, user.email, user.hashed_password, user.profile, user.refresh_token)
+    print(user.id, user.email, user.hashed_password, user.refresh_token)
+user_profiles = session.query(UserProfile).all()
+for user_profile in user_profiles:
+    print(user_profile.id, user_profile.user_id, user_profile.nickname)
