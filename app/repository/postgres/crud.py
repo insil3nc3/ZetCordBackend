7c38c1b9d0b5d1ac6facbcc8a5b6848bcd0ce87d
insil3nc3 <minecrafttvem@gmail.com>
2025-05-10 06:13:49 +0200

    added 'get_user_by_id', 'get_profile_by_unique_name', 'check_for_unique_name_existing', 'get_user_profile_by_id' functions

--- app/repository/postgres/crud.py
+++ app/repository/postgres/crud.py
@@ -11,6 +11,11 @@ async def get_user_by_email(user_email: str, session: AsyncSession) -> Users | N
     if result:
         return result.scalars().first()
 
+async def get_user_by_id(user_id: int, session: AsyncSession) -> Users | None:
+    result = await session.execute(select(Users).where(Users.id == user_id))
+    if result:
+        return result.scalars().first()
+
 async def get_user_profile_by_email(email: str, session: AsyncSession):
     user = await get_user_by_email(email, session)
     result = await session.execute(select(UserProfile).where(UserProfile.user_id == user.id))
@@ -23,3 +28,25 @@ async def get_avatar_by_id(profile_id: int, session: AsyncSession):
     if not profile:
         raise HTTPException(status_code=404, detail="Avatar not found")
     return profile.avatar
+
+async def get_profile_by_unique_name(user_unique_id: str, session: AsyncSession):
+    result = await session.execute(select(UserProfile).where(UserProfile.unique_name == user_unique_id))
+    profile = result.scalars().first()
+    if not profile:
+        raise HTTPException(status_code=404, detail="User not found")
+    return profile
+
+async def check_for_unique_name_existing(user_unique_id: str, session: AsyncSession):
+    result = await session.execute(select(UserProfile).where(UserProfile.unique_name == user_unique_id))
+    profile = result.scalars().first()
+    if not profile:
+        return False
+    if profile:
+        return True
+
+async def get_user_profile_by_id(user_id: int, session: AsyncSession):
+    result = await session.execute(select(UserProfile).where(UserProfile.user_id == user_id))
+    profile = result.scalars().first()
+    if not profile:
+        raise HTTPException(status_code=404, detail="Profile not found")
+    return profile
