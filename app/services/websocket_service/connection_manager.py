1d1c96112187390451be0438a3e14e896296b0ff
insil3nc3 <minecrafttvem@gmail.com>
2025-05-10 06:25:52 +0200

    created class to manage the connection

---
+++ app/services/websocket_service/connection_manager.py
@@ -0,0 +1,26 @@
+from fastapi import WebSocket
+from typing import Dict
+
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: Dict[int, WebSocket] = {}
+
+    async def connect(self, user_id: int, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections[user_id] = websocket
+
+    async def disconnect(self, user_id: int):
+        self.active_connections.pop(user_id, None)
+
+    async def send_json(self, user_id: int, data: dict):
+        ws = self.active_connections.get(user_id)
+        if ws:
+            await ws.send_json(data)
+
+    async def send_personal_message(self, message: dict, user_id: int):
+        websocket = self.active_connections.get(user_id)
+        if websocket:
+            await websocket.send_json(message)
+            print(f"Сообщение отправлено пользователю {user_id}: {message}")
+        else:
+            print(f"Нет активного WebSocket-подключения для пользователя {user_id}")
