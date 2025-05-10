7f0c54dab225f865670d30ac69a87a365aa64006
insil3nc3 <minecrafttvem@gmail.com>
2025-05-10 06:32:34 +0200

    added user, chat, ws router

--- app/main.py
+++ app/main.py
@@ -1,6 +1,13 @@
-import asyncio
-from fastapi import FastAPI, routing, APIRouter
+from fastapi import FastAPI
 from app.api.v1.routers.auth.auth import auth_router
+from app.api.v1.routers.account.profile_actions import user_router
+from app.services.websocket_service.connection_manager import ConnectionManager
+from app.api.v1.routers.chat.private_chat import chat_router
+from app.api.v1.routers.ws.ws import ws_router
 
 app = FastAPI()
+manager = ConnectionManager()
 app.include_router(auth_router)
+app.include_router(user_router)
+app.include_router(chat_router)
+app.include_router(ws_router)
